7f8f2d4cd5506406e1ff94905b58bbbf56139829
qnglndl <fhfhfhfu114514@163.com>
2025-12-24 23:18:25 +0800

    新增启动gui，没完成

---
+++ Familytree_gui/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Familytree_gui
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
